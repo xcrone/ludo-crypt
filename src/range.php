f7adf9882871186cee7b600f5902438f9bb0cf7c
Mohamed Zahiruddin <48623158+xcrone@users.noreply.github.com>
2023-01-15 18:30:42 +0800

    fix

--- src/range.php
+++ src/range.php
@@ -5,7 +5,8 @@ namespace XCrone\LudoEncryption;
 class Range {
     static public function get()
     {
-        $ranges = json_decode(file_get_contents('range.json'));
+        $path = __DIR__ . '/range.json';
+        $ranges = json_decode(file_get_contents($path));
 
         if (! $ranges) return;
 
